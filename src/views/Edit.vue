d651b58112a63a22add8d168a3d7ef0666d49f5c
EderNieves01 <edernieves09@gmail.com>
2023-10-11 17:07:08 -0400

    updateProjectsWithPATCH

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -13,7 +13,7 @@
     
        <div class="row">
         
-        <form class="col s12" @submit.prevent="createdProjects">
+        <form class="col s12" @submit.prevent="updateProjects">
           <div class="row">
             <div class="input-field col s12">
                 <input id="first_name" type="text" class="validate"
@@ -67,8 +67,10 @@
     export default {
         data(){
             return{
-                project: { } //colocamos el objeto vacio para llenarlo con el objeto obtenido
+                project: { }, //colocamos el objeto vacio para llenarlo con el objeto obtenido
                 //de la respuesta en getProjects
+                //creamos el id del objeto a editar
+                id: this.$route.params.id
             }
         },
         mounted(){
@@ -79,14 +81,30 @@
             async getProjects() {
               // con esto this.$route.params.id obtenemos la ruta los parametros y el id que necesitamos
             //mandamos el id como string en la ruta ${id} usando ``
-              const id = this.$route.params.id
+              
                 const data = await fetch(
-               `https://crud-vue-5a4b5-default-rtdb.firebaseio.com/projects/${id}.json`)
+               `https://crud-vue-5a4b5-default-rtdb.firebaseio.com/projects/${this.id}.json`)
            const res = await data.json();
            
            this.project = res;
            
-             }
+             },
+             async updateProjects() {
+          
+          const data = await fetch(`https://crud-vue-5a4b5-default-rtdb.firebaseio.com/projects/${this.id}.json`,//projects.json crea en firebase,
+          {
+      
+           method: "PATCH", //actualiza los datos en firebase
+           //mandamos el objeto en formato objeto json de javascript
+           body: JSON.stringify(this.project)
+           // project: this.project
+          }
+          )
+          console.log(data)
+       //    const res = await data.json()
+          
+           
+       }
         }
     }
     </script>
