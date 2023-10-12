fd4b3d5bfd01250e57212452f7354b6f91a96a03
EderNieves01 <edernieves09@gmail.com>
2023-10-11 23:50:57 -0400

    registerUsers

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="container">
   <div class="row">
-    <form @submit.prevent="" class="col s12">
+    <form @submit.prevent="validarForm()" class="col s12">
       <div class="row">
         <div class="row">
         <div class="input-field col s12">
-          <input id="email" type="email" class="validate">
+            <!-- el .trim no lee los espacios al inicio -->
+          <input v-model.trim="email" id="email" type="email" class="validate">
           <label for="email">Email</label>
         </div>
       </div> 
@@ -13,13 +14,13 @@
      
       <div class="row">
         <div class="input-field col s12">
-          <input id="password" type="password" class="validate">
+          <input v-model.trim="pass1" id="password" type="password" class="validate">
           <label for="password">Password</label>
         </div>
       </div>
       <div class="row">
         <div class="input-field col s12">
-          <input id="passwordConfirmation" type="password" class="validate">
+          <input v-model.trim="pass2" id="passwordConfirmation" type="password" class="validate">
           <label for="passwordConfirmation">Confirm Password</label>
         </div>
       </div>
@@ -32,7 +33,38 @@
 
 <script>
 export default {
-
+data: () => ({
+    email: "",
+    pass1: "",
+    pass2: ""
+}),
+ methods: {
+  async  validarForm(){
+     if (this.pass1 === this.pass2 &&
+         this.pass1.length >= 6 &&
+          this.email != "") {
+            //obtenemos el apikye generado en firebase
+            const APIkey ="AIzaSyDAJrjwFmjewL3HwvpRmNzKW3oQhdUNaBU";
+        const res = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${APIkey}`,
+           {
+            //metodo post para agregar usuarios
+            method: "POST",
+            //mandando los input a firebase
+            body: JSON.stringify({
+                email: this.email,
+                password: this.pass1,
+                //permite obtener el id
+                returnSecureToken: true,
+            }),
+           }
+           )
+           console.log(await res.json())
+        }
+       else {
+            return console.log("contrasenas invalidas")
+        }
+    }
+ }
 }
 </script>
 
